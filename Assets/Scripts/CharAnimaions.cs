5afa851a89d221b9dcdfda4900282e9acaeff6a0
mnagahama <mnagahama21@gmail.com>
2023-10-12 12:07:36 -0300

    maioria das animações ja estao no animator e no script, falta apenas os taunts e os passos

--- Assets/Scripts/CharAnimaions.cs
+++ Assets/Scripts/CharAnimaions.cs
@@ -17,14 +17,11 @@ public class CharAnimaions : MonoBehaviour
             anim.SetInteger("StartPoseIndex", Random.Range(0, 3)); //Ice, Fire, Necro
             anim.SetTrigger("StartPose");
 
-
             anim.SetInteger("BuffIndex", Random.Range(0, 2)); //Normal, Olhando p/ jogador
             anim.SetTrigger("Buff");
 
-
             anim.SetInteger("DebuffIndex", Random.Range(0, 2)); //Normal, Olhando p/ jogador
             anim.SetTrigger("Debuff");
-
 
             anim.SetInteger("IdleIndex", Random.Range(0, 2)); //Normal, Olhando p/ jogador
             anim.SetTrigger("Idle");
@@ -41,6 +38,29 @@ public class CharAnimaions : MonoBehaviour
             anim.SetInteger("VictoryIndex", Random.Range(0, 5)); // Fire, Ice, General, Mockery, Necro
             anim.SetTrigger("Victory");
 
+            anim.SetInteger("TpIndex", Random.Range(0, 3)); // Normal, Praise the sun, nao lembro
+            anim.SetTrigger("Tp");
+
+            anim.SetInteger("AcertouIndex", Random.Range(0, 3)); // Normal, felicidade, Olhando p/ jogador
+            anim.SetTrigger("Acertou");
+
+            anim.SetInteger("ErrouuIndex", Random.Range(0, 3)); // Normal, batendo na cara, Olhando p/ jogador
+            anim.SetTrigger("Errou");
+
+            anim.SetInteger("HNIndex", Random.Range(0, 4)); // Deboche, Recuando, Olhando p/ jogador, Normal
+            anim.SetTrigger("HNormal");
+
+            anim.SetInteger("HUIndex", Random.Range(0, 4)); // Normal, Deboche, Olhando p/ jogador, Taunt
+            anim.SetTrigger("HitUlt");
+
+            anim.SetInteger("FogoIndex", Random.Range(0, 2)); // Normal, Ult
+            anim.SetTrigger("Fogo");
+
+            anim.SetInteger("GeloIndex", Random.Range(0, 2)); // Normal, Ult
+            anim.SetTrigger("Gelo");
+
+            anim.SetInteger("NecroIndex", Random.Range(0, 2)); // Normal, Ult
+            anim.SetTrigger("Necro");
         }
     }
 }
