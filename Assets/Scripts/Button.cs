72b952e391e067287b40b15ae820e9c8c75e6520
mnagahama <mnagahama21@gmail.com>
2023-11-02 19:38:40 -0300

    adicionado os botoes e o menu

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -7,6 +7,27 @@ public class Button : MonoBehaviour
 {
    public void Menu() //Se o botão do menu for apertado, ele carrega a cena do jogo
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Game");
     }
+
+    public void Exit() //Se o botão do exit for apertado, ele fecha o jogo
+    {
+        Application.Quit();
+    }
+
+    public void Credits() //Se o botão do credits for apertado, ele carrega a cena do credits
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Play() //Se o botão do play/playagain for apertado, ele carrega a cena do jogo
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitToMenu() //Se o botão do exittomenu for apertado, ele carrega a cena do menu
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
 }
