53f496fa43073661d03489d1d73cbac86d5d1a89
igorstein97 <igorsteinaa@gmail.com>
2023-11-05 20:35:47 -0300

    DeckSelector
    oi

---
+++ Assets/Deck Selection/ButtomMen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ButtomMen : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Button fire; 
+    [SerializeField] private UnityEngine.UI.Button ice;
+    [SerializeField] private UnityEngine.UI.Button darkMagic;
+    
+    
+    
+}
