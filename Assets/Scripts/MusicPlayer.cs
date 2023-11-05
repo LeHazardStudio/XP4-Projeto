36cee251f4f5a1af64fa6f2b913584422d44f700
VirtuK <vitorkuiava.vc@gmail.com>
2023-11-05 18:24:38 -0300

    Coloquei a musica (A verificar se ficou bom assim)

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource music;
+    public AudioClip intro;
+    public AudioClip loop;
+    public AudioClip final;
+    public JogoManagement jm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jm = FindObjectOfType<JogoManagement>();
+        music.clip = intro;
+        music.loop = false;
+        music.Play();
+       
+    }
+
+    void Update()
+    {
+        if(music.clip == intro && music.isPlaying == false)
+        {
+            music.clip = loop;
+            music.loop = true;
+            music.Play();
+        }
+        if (jm.P1_HP <= 0 || jm.P2_HP <= 0)
+        {
+            music.loop = false;
+            if (music.isPlaying == false)
+            {
+                music.clip = final;
+                music.Play();
+            }
+        }
+    }
+}
+    
+
+    
+
